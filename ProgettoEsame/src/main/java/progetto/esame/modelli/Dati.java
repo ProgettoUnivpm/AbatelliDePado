53df2fe5ee36333469785e818d32876abb46b153
erosa <erosa@LAPTOP-STN0AV1C>
2020-05-20 17:35:47 +0200

    Completamento delle relative classi

--- ProgettoEsame/src/main/java/progetto/esame/modelli/Dati.java
+++ ProgettoEsame/src/main/java/progetto/esame/modelli/Dati.java
@@ -1,5 +1,62 @@
 package progetto.esame.modelli;
 
-public class Dati {
+public class Dati 
+{
+	public int id;
+	public String media_type;
+	public String media_url;
+	public String username;
+	public String timestamp;
+	
+	public Dati(int id, String media_type, String media_url, String username, String timestamp)
+	{
+		this.id = id;
+		this.media_type = media_type;
+		this.media_url = media_url;
+		this.username = username;
+		this.timestamp = timestamp;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMedia_type() {
+		return media_type;
+	}
+
+	public void setMedia_type(String media_type) {
+		this.media_type = media_type;
+	}
+
+	public String getMedia_url() {
+		return media_url;
+	}
+
+	public void setMedia_url(String media_url) {
+		this.media_url = media_url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	
 
 }
--- ProgettoEsame/src/main/java/progetto/esame/modelli/Metadata.java
+++ ProgettoEsame/src/main/java/progetto/esame/modelli/Metadata.java
@@ -1,5 +1,35 @@
 package progetto.esame.modelli;
 
-public class Metadata {
+
+
+
+public class Metadata 
+{
+	public String tipo;
+	public String nome;
+	
+	public Metadata(String nome, String tipo)
+	{
+		this.nome = nome;
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 
 }
